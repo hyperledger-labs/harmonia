a912bc9fdd3e234f4051bd88d8dd37508ec76f0d
Edoardo Ierina <edoardo.ierina@r3.com>
2023-10-17 00:52:06 +0100

    fixes for flows demo
    Signed-off-by: Edoardo Ierina <edoardo.ierina@r3.com>

--- src/r3/atomic-swap/corda/samples/atomic-swap/swap-workflows/src/main/kotlin/com/interop/flows/SignaturesCollectorFlow.kt
+++ src/r3/atomic-swap/corda/samples/atomic-swap/swap-workflows/src/main/kotlin/com/interop/flows/SignaturesCollectorFlow.kt
@@ -9,6 +9,7 @@ import net.corda.core.crypto.SecureHash
 import net.corda.core.flows.*
 import net.corda.core.identity.AbstractParty
 import net.corda.core.serialization.CordaSerializable
+import net.corda.core.utilities.loggerFor
 import net.corda.core.utilities.unwrap
 import org.web3j.utils.Numeric
 import java.math.BigInteger
@@ -50,6 +51,10 @@ object SignaturesCollectorFlow {
         val blocking: Boolean
     ) : FlowLogic<Unit>() {
 
+        companion object {
+            val log = loggerFor<CollectBlockSignaturesFlow>()
+        }
+
         @Suspendable
         override fun call() {
 
@@ -65,22 +70,29 @@ object SignaturesCollectorFlow {
                 serviceHub.identityService.partyFromKey(it)
             }
 
+            log.info("Validators: ${validators.first()}")
+
             val sessions = validators.map { initiateFlow(it) }
 
             for(session in sessions) {
                 try {
+                    log.info("Sent request params $blockNumber $blocking")
                     session.send(RequestParams(blockNumber, blocking))
-                } catch (_: Throwable) {
+                } catch (e: Throwable) {
                     // NOTE: gather as many signatures as possible, ignoring single errors.
+                    log.error("Error while sending response.\nError: $e")
                 }
             }
 
             if(blocking) {
                 for (session in sessions) {
                     try {
+                        log.info("Receiving response")
                         session.receive<Boolean>()
-                    } catch (_: Throwable) {
+                        log.info("Received response")
+                    } catch (e: Throwable) {
                         // NOTE: gather as many signatures as possible, ignoring single errors.
+                        log.error("Error while receiving response.\nError: $e")
                     }
                 }
             }
@@ -92,16 +104,28 @@ object SignaturesCollectorFlow {
     @InitiatedBy(CollectBlockSignaturesFlow::class)
     class CollectBlockSignaturesFlowResponder(val session: FlowSession) : FlowLogic<Unit>() {
 
+        companion object {
+            val log = loggerFor<CollectBlockSignaturesFlowResponder>()
+        }
+
         @Suspendable
         override fun call() {
             // 2. Validator[i] receives a request to sign
             val request = session.receive<RequestParams>().unwrap { it }
+            log.info("Received request params $request.blockNumber $request.blocking")
 
+            log.info("Initiating collector subflow")
             subFlow(CollectorInitiator(session.counterparty, request.blockNumber, request.blocking))
+            log.info("Initiated collector subflow")
 
             if(request.blocking) {
                 // send a dummy response to unblock the initiating flow
-                session.send(true)
+                log.info("Sending dummy response")
+                try {
+                    session.send(true)
+                } catch (e: Throwable) {
+                    log.error("Error while sending response.\nError: $e")
+                }
             }
         }
     }
@@ -121,18 +145,32 @@ object SignaturesCollectorFlow {
         private val blockNumber: BigInteger,
         private val blocking: Boolean
     ) : FlowLogic<Unit>() {
+
+        companion object {
+            val log = loggerFor<CollectorInitiator>()
+        }
+
         @Suspendable
         override fun call() {
             // 3. Validator[i] query the EVM, sign the block, and send the signature to the requesting party
+            log.info("Signing receipt root")
             val signature = signReceiptRoot(blockNumber)
+            log.info("Signed receipt root")
 
             val session = initiateFlow(recipient)
 
+            log.info("Sending request params with signature")
             session.send(RequestParamsWithSignature(blockNumber, blocking, signature))
+            log.info("Sent request params with signature")
 
             if(blocking) {
                 // wait for a dummy response before returning to the caller
-                session.receive<Boolean>()
+                log.info("Waiting dummy response")
+                try {
+                    session.receive<Boolean>()
+                } catch (e: Throwable) {
+                    log.error("Error while receiving response.\nError: $e")
+                }
             }
         }
 
@@ -150,19 +188,35 @@ object SignaturesCollectorFlow {
     @StartableByRPC
     @InitiatedBy(CollectorInitiator::class)
     class CollectorResponder(private val session: FlowSession) : FlowLogic<Unit?>() {
+
+        companion object {
+            val log = loggerFor<CollectorResponder>()
+        }
+
         @Suspendable
         override fun call() {
             // 4. The requesting party stores the signatures
-            val params = session.receive<RequestParamsWithSignature>().unwrap { it }
+            val params = try {
+                session.receive<RequestParamsWithSignature>().unwrap { it }
+            } catch (e: Throwable) {
+                log.error("Error while receiving response.\nError: $e")
+                throw e
+            }
 
+            log.info("Received request params with signature")
+
+            log.info("Saving block signature")
             serviceHub.cordaService(DraftTxService::class.java).saveBlockSignature(
                 params.blockNumber,
                 params.signature
             )
+            log.info("Saved block signature")
 
             if(params.blocking) {
                 // send a dummy response to unblock the initiating flow
+                log.info("Sending dummy response")
                 session.send(true)
+                log.info("Sending dummy response")
             }
         }
     }
@@ -173,7 +227,7 @@ object SignaturesCollectorFlow {
     @Suspendable
     @StartableByRPC
     @InitiatingFlow
-    class DumpInitiator(
+    class S(
         private val blockNumber: BigInteger
     ) : FlowLogic<String>() {
         @Suspendable
